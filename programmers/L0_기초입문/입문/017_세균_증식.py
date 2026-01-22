c93ee94d0c7e7810c355c045dd3f57c2e67af36a
Samju <dsdna12@gmail.com>
2026-01-22 09:13:38 +0900

    [프로그래머스] L0 - 세균 증식 (#17)

---
+++ programmers/L0_기초입문/입문/017_세균_증식.py
@@ -0,0 +1,10 @@
+# 세균 증식
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120910
+# 알고리즘: 기초
+# 작성자: 손삼주
+# 작성일: 2026. 01. 22. 09:13:37
+
+def solution(n, t):
+    answer = n<<t
+    return answer
