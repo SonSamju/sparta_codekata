aedeb203c4230a5c3427e7e6b37da0ba3b300717
Samju <dsdna12@gmail.com>
2026-02-26 11:36:32 +0900

    [프로그래머스] 그룹별 조건에 맞는 식당 목록 출력하기 풀이 제출
    - 문제 ID: 131124
    - 난이도: unknown
    - 제출일: 2026. 02. 26. 11:36:32
    - 작성자: 손삼주

---
+++ programmers/other/131124_그룹별_조건에_맞는_식당_목록_출력하기.py
@@ -0,0 +1,29 @@
+# 그룹별 조건에 맞는 식당 목록 출력하기
+# 프로그래머스 (unknown)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/131124
+# 작성자: 손삼주
+# 작성일: 2026. 02. 26. 11:36:32
+
+-- 코드를 입력하세요
+SELECT
+    M.MEMBER_NAME,
+    R.REVIEW_TEXT,
+    DATE_FORMAT(R.REVIEW_DATE, '%Y-%m-%d') AS REVIEW_DATE
+FROM REST_REVIEW R
+    JOIN MEMBER_PROFILE M
+    ON R.MEMBER_ID = M.MEMBER_ID
+WHERE R.MEMBER_ID IN (
+    SELECT MEMBER_ID
+    FROM REST_REVIEW
+    GROUP BY MEMBER_ID
+    HAVING COUNT(REVIEW_ID) = (
+        SELECT COUNT(REVIEW_ID) 
+        FROM REST_REVIEW 
+        GROUP BY MEMBER_ID 
+        ORDER BY COUNT(REVIEW_ID) DESC
+        LIMIT 1)
+    )
+ORDER BY REVIEW_DATE ASC, REVIEW_TEXT ASC
+
+
+-- HAVING COUNT(REVIEW_ID) = MAX(COUNT(REVIEW_ID))
