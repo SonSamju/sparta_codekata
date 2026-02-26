19aab85a0a20b0f96dee856a68fa2cccd40e84de
Samju <dsdna12@gmail.com>
2026-02-26 10:28:17 +0900

    [프로그래머스] 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기 풀이 제출
    - 문제 ID: 164671
    - 난이도: unknown
    - 제출일: 2026. 02. 26. 10:28:16
    - 작성자: 손삼주

---
+++ programmers/other/164671_조회수가_가장_많은_중고거래_게시판의_첨부파일_조회하기.py
@@ -0,0 +1,14 @@
+# 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기
+# 프로그래머스 (unknown)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/164671
+# 작성자: 손삼주
+# 작성일: 2026. 02. 26. 10:28:16
+
+-- 파일경로: /home/grep/src/BOARD_ID/FILE_IDFILE_NAME.FILE_EXT
+SELECT CONCAT('/home/grep/src/', BOARD_ID, '/', FILE_ID, FILE_NAME, FILE_EXT) AS FILE_PATH
+FROM USED_GOODS_FILE
+WHERE BOARD_ID = (SELECT BOARD_ID
+FROM USED_GOODS_BOARD
+WHERE VIEWS = (SELECT MAX(VIEWS) FROM USED_GOODS_BOARD)
+)
+ORDER BY FILE_ID DESC
