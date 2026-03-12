556fefb3ad2916e8aad05b9434213b7d232ef4f6
Samju <dsdna12@gmail.com>
2026-03-12 15:45:37 +0900

    [프로그래머스] 상품을 구매한 회원 비율 구하기 풀이 제출
    - 문제 ID: 131534
    - 난이도: unknown
    - 제출일: 2026. 03. 12. 15:45:36
    - 작성자: 손삼주

--- programmers/other/131534_상품을_구매한_회원_비율_구하기.py
+++ programmers/other/131534_상품을_구매한_회원_비율_구하기.py
@@ -2,7 +2,7 @@
 # 프로그래머스 (unknown)
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/131534
 # 작성자: 손삼주
-# 작성일: 2026. 03. 12. 15:45:18
+# 작성일: 2026. 03. 12. 15:45:36
 
 SELECT
     YEAR(SALES_DATE) AS YEAR,
