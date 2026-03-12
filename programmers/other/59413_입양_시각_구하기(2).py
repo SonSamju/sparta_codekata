cb5621ec172c0a7e98d761c6e93fc3ebdb9ec92e
Samju <dsdna12@gmail.com>
2026-03-12 15:10:27 +0900

    [프로그래머스] 입양 시각 구하기(2) 풀이 제출
    - 문제 ID: 59413
    - 난이도: unknown
    - 제출일: 2026. 03. 12. 15:10:26
    - 작성자: 손삼주

---
+++ programmers/other/59413_입양_시각_구하기(2).py
@@ -0,0 +1,30 @@
+# 입양 시각 구하기(2)
+# 프로그래머스 (unknown)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/59413
+# 작성자: 손삼주
+# 작성일: 2026. 03. 12. 15:10:26
+
+WITH RECURSIVE cte AS (
+    SELECT 0 as HOUR
+    UNION ALL
+    SELECT HOUR+1 AS HOUR 
+    FROM cte
+    WHERE HOUR < 23
+)
+SELECT
+    c.HOUR,
+    IFNULL(COUNT, 0) AS COUNT
+FROM cte c 
+LEFT JOIN (
+    SELECT 
+    HOUR(DATETIME) AS HOUR,
+    COUNT(ANIMAL_ID) AS COUNT
+    FROM ANIMAL_OUTS
+    GROUP BY HOUR
+    ) AS a
+    ON c.HOUR = a.HOUR
+ORDER BY HOUR
+    
+
+
+
